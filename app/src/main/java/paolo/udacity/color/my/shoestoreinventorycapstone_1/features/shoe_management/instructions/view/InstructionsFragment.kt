258641366db49c322c9e7ae8d0cdf3aa316331f9
antaresr <diagonal_zero@hotmail.com>
2021-04-07 15:02:43 -0500

    Only missing style abstraction

--- app/src/main/java/paolo/udacity/color/my/shoestoreinventorycapstone_1/features/shoe_management/instructions/view/InstructionsFragment.kt
+++ app/src/main/java/paolo/udacity/color/my/shoestoreinventorycapstone_1/features/shoe_management/instructions/view/InstructionsFragment.kt
@@ -24,7 +24,7 @@ class InstructionsFragment : Fragment() {
     }
     private lateinit var binding: FragmentInstructionsBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
         initUi()
         return binding.root
